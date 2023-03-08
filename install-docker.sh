49d3827691756f0144f33d0da4c559dbcdfe3260
philipp1992 <mail@pm92.de>
2023-03-08 13:55:46 +0100

    init

---
+++ install-docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# update OS 
+apt-get update
+apt-get upgrade -y
+
+# install dependencies
+apt-get install  ca-certificates  curl  gnupg  lsb-release -y
+
+# add docker gpg key
+mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
+
+# setup docker repository
+echo  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# update repo index
+apt-get update
+
+# install docker
+apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-compose -y
+
+# verify installation
+docker run hello-world
