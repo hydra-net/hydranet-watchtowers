b9e06d30e74e234bbd5f6266d605b6108a7a4322
philipp1992 <mail@pm92.de>
2023-03-13 13:52:43 +0100

    wip

--- get-connext-address.sh
+++ get-connext-address.sh
@@ -1,3 +1,5 @@
 port=$(cat config.yaml  |grep "port: " |cut -d " " -f 2)
 addr=$(curl -s -k https://127.0.0.1:${port}/address)
 echo "Connext watchtowers public address is ${addr}. Use this to top up with funds."
+ip=$(curl -s ident.me)
+echo "Your connext watchtower url is: https://${ip}:${port}. Use the api key from the config.yaml"
